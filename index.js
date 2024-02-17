8b505e49b2b7d4d29cbdd8af2af27c036797e7ec
Mardin <mardinomer001@gmail.com>
2024-02-17 12:59:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+// index.js
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function appendCat(name) {
+  return cats.concat([name]);
+}
+
+function prependCat(name) {
+  return [name].concat(cats);
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+module.exports = {
+  cats,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  destructivelyAppendCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat
+};
